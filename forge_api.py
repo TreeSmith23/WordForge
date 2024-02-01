6f7db171e027cddafe54eaf34a49d810d3793c35
totreysmith <totreysmith@tamu.edu>
2024-01-31 23:21:42 -0600

    removed all 2 letter words

--- forge_api.py
+++ forge_api.py
@@ -6,6 +6,13 @@ from flask_cors import CORS
 
 def main():
     #Code here
+    with open("new_english_words_full.txt", 'w') as wfile:
+        with open("english_words_full.txt", 'r') as file:
+                for line in file:
+                    if len(line.strip()) > 2:
+                         wfile.write(line)
+                     
+                
     return 0
 
 if __name__ == "__main__":
